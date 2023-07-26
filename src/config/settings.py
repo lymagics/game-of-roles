678393bd010e34351325e9dbaea7a0fc22cf090f
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-26 17:15:50 +0300

    feat(rest-setup): add default pagination.

--- src/config/settings.py
+++ src/config/settings.py
@@ -115,6 +115,8 @@ CSRF_TRUSTED_ORIGINS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
