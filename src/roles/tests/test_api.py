da6a45daa9be4c2c79bb8413a9a165b6576021e8
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-27 12:51:34 +0300

    test: add tests for checkout endpoint.

--- src/roles/tests/test_api.py
+++ src/roles/tests/test_api.py
@@ -13,6 +13,7 @@ class TestRoleAPI(APITestCase):
     def setUpTestData(cls):
         cls.user = UserFactory()
         cls.url = reverse('api:roles:roles-list')
+        cls.url_purchase = reverse('api:roles:roles-purchase')
 
     def test_retrieving_list_of_roles(self):
         self.client.force_authenticate(user=self.user)
@@ -20,3 +21,21 @@ class TestRoleAPI(APITestCase):
         self.assertEqual(response.status_code, 200)
         for role in Role.objects.all():
             self.assertContains(response, role)
+
+    def test_generating_stripe_checkout_url(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url_purchase, {
+            'name': 'Premium',
+            'interval': 3,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'url')
+
+    def test_not_generating_stripe_checkout_url(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url_purchase, {
+            'name': 'Premium',
+            'interval': 4,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()['interval'], ['"4" is not a valid choice.'])
