fb184cdc879cf88a7fd37f750da567b5aadbe36f
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-07-27 10:26:59 +0300

    feat(subscriptions): add method to get checkout url.

--- src/roles/models.py
+++ src/roles/models.py
@@ -1,5 +1,9 @@
+import stripe
+
 from django.conf import settings
 from django.db import models
+
+stripe.api_key = settings.STRIPE_API_KEY
 
 
 class IntervalChoices(models.IntegerChoices):
@@ -18,6 +22,32 @@ class Role(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(default='', blank=True)
     price_per_month = models.IntegerField()
+
+    def to_checkout(self, user, interval):
+        return stripe.checkout.Session.create(
+            mode='subscription',
+            success_url=settings.PAYMENT_SUCCESS_URL,
+            cancel_url=settings.PAYMENT_CANCEL_URL,
+            metadata={
+                'user_id': user.pk,
+                'role': self.name,
+            },
+            line_items=[{
+                'quantity': 1,
+                'price_data': {
+                    'currency': settings.CURRENCY,
+                    'unit_amount': self.price_per_month,
+                    'product_data': {
+                        'name': self.name,
+                        'description': self.description,
+                    },
+                    'recurring': {
+                        'interval': 'month',
+                        'interval_count': interval,
+                    },
+                },
+            }],
+        ).url
 
     def __str__(self) -> str:
         return self.name
